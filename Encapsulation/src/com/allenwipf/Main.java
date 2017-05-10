59b7371a9a94c99199b30e2bbc01cffadfc00160
Allen Wipf <allenwipf@msn.com>
2017-05-09 22:26:30 -0500

    encapsulation

--- Encapsulation/src/com/allenwipf/Main.java
+++ Encapsulation/src/com/allenwipf/Main.java
@@ -3,6 +3,20 @@ package com.allenwipf;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+//        Player player = new Player();
+//        player.fullname = "Allen";
+//        player.health = 20;
+//        player.weapon = "Scar";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health is " + player.healthRemaining());
+//
+//        damage = 11;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health is " + player.healthRemaining());
+
+        EnhancedPlayer player = new EnhancedPlayer("Allen", 50, "Scar");
+        System.out.println("Initial health is " + player.getHealth());
     }
 }
