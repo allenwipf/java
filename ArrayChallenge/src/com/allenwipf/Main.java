c58fd3ab0cee105a7ef6d84d9df247a1c63f0b06
Allen Wipf <allenwipf@msn.com>
2017-05-10 23:25:17 -0500

    finished with challenge

--- ArrayChallenge/src/com/allenwipf/Main.java
+++ ArrayChallenge/src/com/allenwipf/Main.java
@@ -1,19 +1,31 @@
 package com.allenwipf;
 
+
+import java.util.Scanner;
+
 public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         int[] myIntegers = getIntegers(5);
         int[] sorted = sortNumbers(myIntegers);
 
+        System.out.println("\nThanks! Below are your numbers sorted.");
+
         printArray(sorted);
     }
-
-    public static int[] getIntegers(int numbers){
-        int[] myArray = {6,4,6,2,6,4};
+    // gets the numbers from keyboard and puts into array
+    public static int[] getIntegers(int numberOfInts){
+        int[] myArray = new int[numberOfInts];
+        System.out.println("Enter " + numberOfInts + " Integers in random order.\r");
+        for (int i = 0; i < numberOfInts; i++){
+            myArray[i] = scanner.nextInt();
+        }
         return myArray;
     }
 
+    // sorts an array of ints from lowest to highest. breaks if there is a 0
     public static int[] sortNumbers(int[] array){
         int[] sortedArray = new int[array.length];
 
@@ -25,7 +37,6 @@ public class Main {
                     position++;
                 }
             }
-            System.out.println("postion " + position + "goes " + array[i]);
             while (sortedArray[position] == array[i]){
                 position--;
             }
@@ -33,7 +44,7 @@ public class Main {
         }
         return sortedArray;
     }
-
+    // prints the sorted array
     public static void printArray(int[] array){
 
         for (int i = 0; i < array.length; i++){
